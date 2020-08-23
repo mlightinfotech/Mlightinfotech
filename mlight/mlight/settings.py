ec6ad6cabd58fd74d446ea490c019b5bbd64276e
mlightinfotech <web.mlightinfotech@gmail.com>
2020-08-23 18:20:51 +0530

    all

--- mlight/mlight/settings.py
+++ mlight/mlight/settings.py
@@ -48,8 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'mlight.urls'
 
 TEMPLATES = [
